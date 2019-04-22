8a27a69feba601f8c81ea3bd893c7bd1a453d579
Boomaa23 <cmo93003@yahoo.com>
2019-04-21 20:50:25 -0700

    Uses OAuth

--- login.php
+++ login.php
@@ -2,19 +2,34 @@
 <html>
 <body>
   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    Name: <input type="text" id="name" name="name"></input>
-    Token: <input type="password" id="token" name="token"></input>
-    <input type="submit">
+    <?php 
+      if(isset($_POST['name'])) {
+        ftruncate(fopen('name.js', "r+"), 0);
+        file_put_contents("name.js", 'const nameGlobal = "' . $_POST['name'] . '";');
+        header("refresh:0;url=index.html");
+      }
+      
+      if(isset($_GET['access_token'])) {
+        if(file_exists('dotenv.js')) {
+          ftruncate(fopen('dotenv.js', "r+"), 0);
+        }
+        file_put_contents("dotenv.js", 'const token = "token=' . $_GET['access_token'] . '";');
+        echo '<a>Access token found - Please enter your name:</a><br />';
+        echo 'Name: <input type="text" id="name" name="name"></input>';
+      } else {
+        echo '<a>Please log in using your access token or OAuth:</a><br />';
+        echo 'Token: <input type="text" id="token" name="token"></input>';
+      }
+    ?>
+    <input type="submit"><a>
+    <?php 
+    if(!isset($_GET['access_token'])) {
+      
+     echo '<b> OR </b></a><a href="https://oauth.groupme.com/oauth/authorize?client_id=3iOCIP8KHemfBNPgYtckc3vcfjNKb17adgj0fBHMpLR9l1CF" target="_blank">OAuth Auto-Login</a>';
+   }
+    ?>
   </form>
+  
 </body>
 </html>
 
-<?php 
-  if(isset($_POST['token'])) {
-    ftruncate(fopen('dotenv.js', "r+"), 0);
-    ftruncate(fopen('name.js', "r+"), 0);
-    file_put_contents("dotenv.js", 'const token = "token=' . $_POST['token'] . '";');
-    file_put_contents("name.js", 'const nameGlobal = "' . $_POST['name'] . '";');
-    header("refresh:0;url=index.html");
-  }
-?>
