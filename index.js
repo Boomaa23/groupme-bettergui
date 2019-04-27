ba4ac325f35b125dcae9dea56d4c6879cc409b0e
Boomaa23 <cmo93003@yahoo.com>
2019-04-26 20:49:42 -0700

    More IP security

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function getIP() {
       var lastip = resp[resp.length-1].substring(1, resp[resp.length-1].length - 1).trim();
       console.log(lastip);
       if(lastip != ip) {
-        window.location.href = "login.php?badip=" + ip + "&goodip=" + lastip;
+        window.location.href = "login.php?badip";
       }
     }
   }
