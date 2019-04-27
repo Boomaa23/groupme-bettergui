36aac15f3a0d7c8dec083e923cce52f8a205039f
Boomaa23 <cmo93003@yahoo.com>
2019-04-26 20:51:34 -0700

    Changes bad IP behavior

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
 <body>
   <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
     <?php 
-      if(isset($_POST['logout'])) {
+      if(isset($_POST['logout']) || isset($_GET['badip'])) {
         file_put_contents("dotenv.js","");
       }
       if(isset($_GET['access_token']) || isset($_POST['token'])) {
